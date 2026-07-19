a85588bab17fdc1fbeb3b13b86f0a930e8b79be4
Antal Robert-Alexandru <96589457+AlexCrominus@users.noreply.github.com>
2026-07-19 13:50:52 +0200

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        maxProfit = 0
+
+        minBuy = 100
+        minBuyIndex = 0
+
+        maxSell = 0
+        maxSellIndex = len(prices)-1
+
+        for i, price in enumerate(prices):
+            if price < minBuy:
+                minBuy = price
+                minBuyIndex = i
+            elif price - minBuy > maxProfit and i > minBuyIndex:
+                maxSell = price
+                maxProfit = maxSell - minBuy
+                maxSellIndex = i
+        # maxProfit = maxSell - minBuy
+        # if maxSell - minBuy > maxProfit and maxSellIndex > minBuyIndex:
+        #         maxProfit = maxSell - minBuy
+        if maxProfit < 0:
+            return 0
+        return maxProfit
+
+            
