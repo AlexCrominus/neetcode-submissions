fa2e80d59f78c958ea65c3aca8476accc39c20b7
Antal Robert-Alexandru <96589457+AlexCrominus@users.noreply.github.com>
2026-07-24 00:19:23 +0200

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        ret = ""
+
+        for n in strs:
+            ret += str(len(n))+"."+n
+
+        return ret
+
+    def decode(self, s: str) -> List[str]:
+        if len(s) == 0:
+            return []
+        ret = []
+        i=0
+        length=""
+        sw = True
+        print(s)
+        while i != len(s):
+            if not sw:
+
+                print(length)
+                word = ""
+                length = int(length)
+                for _ in range(length):
+                    word += s[i]
+                    i+=1 
+                print(word)
+                ret.append(word)
+                sw = True
+                length = ""
+            elif s[i] != "." and sw:
+                length += s[i]
+                i += 1
+            else: 
+                sw = False
+                i+=1
+        if s[i-1] == ".":
+            ret.append("")
+        
+        return ret
