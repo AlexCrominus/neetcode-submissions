d48370e3cbdbe296c286432f5c2c27b3b69946fc
Antal Robert-Alexandru <96589457+AlexCrominus@users.noreply.github.com>
2026-08-15 16:56:49 +0300

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        farthest = 0
+        jumps = 0
+        stack = []
+        mostPot = 0
+
+        if len(nums) == 1:
+            return 0
+        
+
+        for i,n in enumerate(nums):
+            if farthest < i:
+                farthest = mostPot
+                jumps += 1
+            if farthest < i+n:
+                mostPot = max(mostPot, i+n)
+
+
+            if farthest >= len(nums)-1:
+                return jumps
+
+        return jumps
